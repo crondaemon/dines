0eb8b9d8b1f9cedb78c6e378ee60801ef8d516ee
lomato@gmail.com <lomato@gmail.com@5f990396-01c2-d23c-b931-de5e172bf4ea>
2010-04-17 23:26:10 +0000

    Missing files.

---
+++ tokenizer.cpp
@@ -0,0 +1,25 @@
+
+#include "tokenizer.hpp"
+
+std::vector<std::string> tokenize(const std::string str, const std::string sep)
+{
+    std::vector<std::string> result;
+
+    // Skip delimiters at beginning.
+    std::string::size_type lastPos = str.find_first_not_of(sep, 0);
+    // Find first "non-delimiter".
+    std::string::size_type pos = str.find_first_of(sep, lastPos);
+
+    while (std::string::npos != pos || std::string::npos != lastPos) {
+        // Found a token, add it to the vector.
+        result.push_back(str.substr(lastPos, pos - lastPos));
+        
+        // Skip delimiters.  Note the "not_of"
+        lastPos = str.find_first_not_of(sep, pos);
+        // Find next "non-delimiter"
+        pos = str.find_first_of(sep, lastPos);
+    }
+    
+    return result;
+}
+
---
+++ tokenizer.hpp
@@ -0,0 +1,10 @@
+
+#ifndef __TOKENIZER_HPP__
+#define __TOKENIZER_HPP__
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> tokenize(const std::string str, const std::string sep);
+
+#endif
